b9ff825f8baf42f536e3143ce3e491037ba3a624
stedy <zstednic@fhcrc.org>
2016-07-03 21:48:55 -0700

    Now calculating rolling mean and plotting, need better initial value

--- prep_for_d3_demo.R
+++ prep_for_d3_demo.R
@@ -14,7 +14,7 @@ my_data$date <- unlist(strsplit(my_data$date, ":[0-9][0-9] [AP]M"))
 
 #for demo lets use Key Arena to 2nd & Pine
 
-my_trip_data <- 
+my_trip_data <-
   my_data %>% subset(start == "Key Arena / 1st Ave N & Harrison St" & end == "2nd Ave & Pine St") %>%
   rename(starttime = date) %>%
   select(starttime, tripduration) %>%
@@ -25,5 +25,8 @@ trip_data <- read.csv("~/pronto-stories/data/2015_trip_data.csv") %>%
   select(starttime, tripduration) %>%
   mutate(group = "All Riders")
 
-writeout <- rbind(my_trip_data, trip_data)
+writeout <- rbind(my_trip_data, trip_data) %>%
+  mutate(starttime = as.POSIXct(strptime(starttime, "%m/%d/%Y %H:%M"))) %>%
+  arrange(starttime)
+
 write.table(writeout, "d3_demo_data.tsv", sep="\t", row.names=F, quote=F)
