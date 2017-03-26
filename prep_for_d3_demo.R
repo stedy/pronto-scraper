79d809b6c265c2bc1c43a655f81925c8dfbf5a14
stedy <zstednic@fhcrc.org>
2017-03-26 16:15:45 -0700

    better plotting, adjust file paths

--- prep_for_d3_demo.R
+++ prep_for_d3_demo.R
@@ -12,7 +12,7 @@ my_data <- data.frame(date = ul[seq(1, lul, by=4)],
 my_data[] <- sapply(my_data, as.character)
 my_data$date <- unlist(strsplit(my_data$date, ":[0-9][0-9] [AP]M"))
 
-#for demo lets a common route
+#lets demo with a common route
 
 my_trip_data <-
   my_data %>% subset(start == "Fred Hutchinson Cancer Research Center / Fairview Ave N & Ward St" & end == "Westlake Ave & 6th Ave") %>%
@@ -21,9 +21,9 @@ my_trip_data <-
   select(starttime, tripduration) %>%
   mutate(group = "My data")
 
-trip_data <- read.csv("~/pronto-stories/data/2015_trip_data.csv") %>%
+trip_data <- read.csv("2016-12_trip_data.csv") %>%
   subset(from_station_id == "EL-01" & to_station_id == "SLU-15" &
-           usertype == "Annual Member") %>%
+           usertype == "Member") %>%
   select(starttime, tripduration) %>%
   mutate(group = "All Riders")
 
@@ -31,6 +31,8 @@ writeout <- rbind(my_trip_data, trip_data) %>%
   mutate(starttime = as.POSIXct(strptime(starttime, "%m/%d/%Y %H:%M"))) %>%
   mutate(my_besttime = min(my_trip_data$tripduration),
          route_besttime = min(trip_data$tripduration)) %>%
+  mutate(drop = ifelse(round(tripduration) %in% my_trip_data$tripduration & group == "All Riders", F, T)) %>%
+  subset(drop == T) %>%
   arrange(starttime)
 
 write.table(writeout, "d3_demo_data.tsv", sep="\t", row.names=F, quote=F)
